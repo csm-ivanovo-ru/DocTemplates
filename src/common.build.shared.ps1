4715f120c637d9fc4ef87de332bf81ee33966da0
Sergei S Betke <sergey.s.betke@yandex.ru>
2022-05-17 02:13:10 +0300

    ci: :recycle: Move nuget build task script to tools\nuget subfolder

--- src/common.build.shared.ps1
+++ src/common.build.shared.ps1
@@ -46,11 +46,7 @@ if ( -not ( Test-Path variable:RepoRootPath ) -or ( [System.String]::IsNullOrEmp
 [System.String] $ToolsPath = ( Join-Path -Path $RepoRootPath -ChildPath 'tools' -Resolve );
 
 [System.String] $NuGetToolsPath = ( Join-Path -Path $ToolsPath -ChildPath 'nuget' -Resolve );
-[System.String] $NuGetPath = ( Join-Path -Path $NuGetToolsPath -ChildPath 'nuget.exe' );
-if ( Get-Command 'nuget.exe' -ErrorAction 'SilentlyContinue' )
-{
-	$NuGetPath = 'nuget.exe';
-};
+. $NuGetToolsPath/.build.ps1;
 
 [System.String] $BuildToolsPath = ( Join-Path -Path $ToolsPath -ChildPath 'build' -Resolve );
 [System.String] $UpdateFileLastWriteTimePath = ( Join-Path -Path $BuildToolsPath -ChildPath 'Update-FileLastWriteTime.ps1' -Resolve );
@@ -237,20 +233,6 @@ $JobOpenFile = {
 			$Shell.ShellExecute( $_.FullName, $null, $_.Directory.FullName, $verb, $WindowState );
 		};
 	};
-};
-
-
-[System.String] $NuGetPath = ( Join-Path -Path $NuGetToolsPath -ChildPath 'nuget.exe' );
-
-
-task nuget `
-	-If { -not ( Get-Command $NuGetPath -ErrorAction 'SilentlyContinue' ) } `
-	-Jobs {
-	$NuGetURI = 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe';
-	Invoke-WebRequest $NuGetURI -OutFile $NuGetPath `
-		-Verbose:( $VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue ) `
-		-Debug:( $DebugPreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue );
-	. $NuGetPath sources add -Name 'nuget.org' -Source 'https://api.nuget.org/v3/index.json';
 };
 
 
---
+++ tools/nuget/.build.ps1
@@ -0,0 +1,25 @@
+﻿# Copyright © 2020 Sergei S. Betke
+
+#Requires -Version 5.0
+#Requires -Modules InvokeBuild
+
+Set-StrictMode -Version Latest;
+
+[System.String] $NuGetVersion = 'latest';
+
+[System.String] $NuGetPath = ( Join-Path -Path $PSScriptRoot -ChildPath 'nuget.exe' );
+if ( Get-Command 'nuget.exe' -ErrorAction 'SilentlyContinue' )
+{
+	$NuGetPath = 'nuget.exe';
+};
+
+
+task nuget `
+	-If { -not ( Get-Command $NuGetPath -ErrorAction 'SilentlyContinue' ) } `
+	-Jobs {
+	$NuGetURI = "https://dist.nuget.org/win-x86-commandline/$NuGetVersion/nuget.exe";
+	Invoke-WebRequest $NuGetURI -OutFile $NuGetPath `
+		-Verbose:( $VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue ) `
+		-Debug:( $DebugPreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue );
+	. $NuGetPath sources add -Name 'nuget.org' -Source 'https://api.nuget.org/v3/index.json';
+};
