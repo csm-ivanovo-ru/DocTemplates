0bdd9e85104a7c5ca886a6dc7c62dbf7669c8876
Sergei S Betke <sergey.s.betke@yandex.ru>
2024-08-10 13:57:24 +0300

    refactor(deps): :heavy_plus_sign: replace GitVersion with npx absolute-version
    #120

--- tools/GitVersion/Get-GitVersionSemVer.ps1
+++ tools/GitVersion/Get-GitVersionSemVer.ps1
@@ -14,16 +14,6 @@ Param()
 Set-StrictMode -Version Latest;
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop;
 
-[System.String] $GitVersionPath = (
-	Select-Xml -LiteralPath "$PSScriptRoot/packages.config" `
-		-XPath 'packages/package[ @id = "GitVersion.CommandLine" ]' |
-	Select-Object -ExpandProperty Node -First 1 |
-	ForEach-Object {
-		"$PSScriptRoot/packages/$( $_.id ).$( $_.version )/tools/gitversion.exe"
-	} |
-	Resolve-Path
-).Path;
-
-[System.String] $ProjectVersion = ( . $GitVersionPath /output json /showvariable SemVer );
+[System.String] $ProjectVersion = ( . npx absolute-version --tagGlob "[0-9]*");
 
 return $ProjectVersion;
