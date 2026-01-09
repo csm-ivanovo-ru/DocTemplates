354e8f60d1581303af9c0741cd4642eebe3be15f
sergey-s-betke <sergey.s.betke@yandex.ru>
2026-01-10 00:24:28 +0300

    build: minify png and svg
    Remove transparent background.
    And other minifications with `gulp-imagemin`.

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -17,12 +17,13 @@ import { versionFromGitTag } from 'absolute-version';
 import SaxonJS from 'saxon-js';
 import { url2qr } from 'gulp-file2qr';
 import { sharp2 } from 'gulp-sharp2';
+import imagemin, { optipng, svgo } from 'gulp-imagemin';
 
 //#region вычисление версии
 if (process.env.version) {
-	const version = process.env.version;
+  const version = process.env.version;
 } else {
-	const version = versionFromGitTag({ tagGlob: '[0-9]*' });
+  const version = versionFromGitTag({ tagGlob: '[0-9]*' });
 };
 //#endregion вычисление версии
 
@@ -78,35 +79,35 @@ const DocsXSLTToolsPath = path.join(DocsToolsPath, 'xslt');
 //#region подготовка QR-кодов для URL
 
 const URI_QRCodesConfig = {
-	URLPath: sourceURIsPath,
-	URLSrc: path.join(sourceURIsPath, '*.url'),
-	QRCodesPath: destinationImagesPath,
-	extname: '.png',
-	imageConfig: {
-		type: 'png',
-		scale: 1
-	}
+  URLPath: sourceURIsPath,
+  URLSrc: path.join(sourceURIsPath, '*.url'),
+  QRCodesPath: destinationImagesPath,
+  extname: '.png',
+  imageConfig: {
+    type: 'png',
+    scale: 1
+  }
 };
 
 task('build:images:URL-QRCodes', function () {
-	return src(URI_QRCodesConfig.URLSrc, { encoding: 'utf8' })
-		.pipe(newer({
-			dest: URI_QRCodesConfig.QRCodesPath,
-			ext: URI_QRCodesConfig.extname
-		}))
-		.pipe(url2qr({ qrOptions: URI_QRCodesConfig.imageConfig }))
-		.pipe(dest(URI_QRCodesConfig.QRCodesPath))
+  return src(URI_QRCodesConfig.URLSrc, { encoding: 'utf8' })
+    .pipe(newer({
+      dest: URI_QRCodesConfig.QRCodesPath,
+      ext: URI_QRCodesConfig.extname
+    }))
+    .pipe(url2qr({ qrOptions: URI_QRCodesConfig.imageConfig }))
+    .pipe(dest(URI_QRCodesConfig.QRCodesPath))
 });
 
 task('maintainer-clean:URL-QRCodes',
-	function () {
-		return src(URI_QRCodesConfig.URLSrc, { read: false, allowEmpty: true, base: process.cwd() })
-			.pipe(rename({
-				dirname: URI_QRCodesConfig.QRCodesPath,
-				extname: URI_QRCodesConfig.extname
-			}))
-			.pipe(clean())
-	}
+  function () {
+    return src(URI_QRCodesConfig.URLSrc, { read: false, allowEmpty: true, base: process.cwd() })
+      .pipe(rename({
+        dirname: URI_QRCodesConfig.QRCodesPath,
+        extname: URI_QRCodesConfig.extname
+      }))
+      .pipe(clean())
+  }
 );
 
 //#endregion подготовка QR-кодов для URL
@@ -114,95 +115,104 @@ task('maintainer-clean:URL-QRCodes',
 //#region подготовка изображений
 
 const imagesConfig = {
-	SVGPath: path.join(imagesPath, 'svg'),
-	PNGPath: imagesPNGPath,
-	preprocessedPath: path.join(tempPath, 'images')
+  SVGPath: path.join(imagesPath, 'svg'),
+  PNGPath: imagesPNGPath,
+  preprocessedPath: path.join(tempPath, 'images')
 };
 
 task('build:images:SVG2PNG',
-	function () {
-		return src(
-			path.join(imagesConfig.SVGPath, '*.svg'),
-			{ encoding: false }
-		)
-			.pipe(newer({ dest: imagesConfig.PNGPath }))
-			.pipe(sharp2((sharpObject) => sharpObject
-				.resize({ width: 600 })
-				.toColorspace('b-w')
-				.png({
-					compressionLevel: 9,
-					colors: 2
-				}),
-				{
-					sharpOptions: {
-						density: 600,
-						ignoreIcc: true
-					}
-				}
-			))
-			.pipe(rename({ extname: '.png' }))
-			.pipe(dest(imagesConfig.PNGPath))
-	}
+  function () {
+    return src(
+      path.join(imagesConfig.SVGPath, '*.svg'),
+      { encoding: false }
+    )
+      .pipe(newer({ dest: imagesConfig.PNGPath }))
+      .pipe(sharp2((sharpObject) => sharpObject
+        .flatten({ background: '#ffffff' })
+        .resize({ width: 600 })
+        .toColorspace('b-w')
+        .png({
+          compressionLevel: 9,
+          colors: 2
+        }),
+        {
+          sharpOptions: {
+            density: 600,
+            ignoreIcc: true
+          }
+        }
+      ))
+      .pipe(rename({ extname: '.png' }))
+      .pipe(dest(imagesConfig.PNGPath))
+  }
 );
 
 task('build:images:russian_emblem.svg',
-	function () {
-		return src(path.join(imagesPath, 'russian-emblems/emblem_black_bordered.svg'), { encoding: false })
-			.pipe(newer({
-				dest: imagesConfig.SVGPath,
-				map: () => 'russian_emblem.svg'
-			}))
-			.pipe(rename({ basename: 'russian_emblem' }))
-			.pipe(dest(imagesConfig.SVGPath))
-	}
+  function () {
+    return src(path.join(imagesPath, 'russian-emblems/emblem_black_bordered.svg'), { encoding: false })
+      .pipe(newer({
+        dest: imagesConfig.SVGPath,
+        map: () => 'russian_emblem.svg'
+      }))
+      .pipe(rename({ basename: 'russian_emblem' }))
+      .pipe(dest(imagesConfig.SVGPath))
+  }
 );
 
 task('build:images:preprocessing:PNG',
-	function () {
-		return src(path.join(imagesConfig.PNGPath, '*.png'), { encoding: false })
-			.pipe(newer({ dest: imagesConfig.preprocessedPath }))
-			.pipe(dest(imagesConfig.preprocessedPath))
-	}
+  function () {
+    return src(path.join(imagesConfig.PNGPath, '*.png'), { encoding: false })
+      .pipe(newer({ dest: imagesConfig.preprocessedPath }))
+      .pipe(imagemin([
+        optipng({
+          optimizationLevel: 7,
+        })
+      ]))
+      .pipe(dest(imagesConfig.preprocessedPath))
+  }
 );
 
 task('build:images:preprocessing:SVG',
-	function () {
-		return src(path.join(imagesConfig.SVGPath, '*.svg'), { encoding: false })
-			.pipe(newer({ dest: imagesConfig.preprocessedPath }))
-			.pipe(svgMin((file) => {
-				if (path.matchesGlob(file.basename, 'russian_emblem.svg')) {
-					return {};
-				} else {
-					return {
-						floatPrecision: 0
-					}
-				}
-			}))
-			.pipe(dest(imagesConfig.preprocessedPath))
-	}
+  function () {
+    return src(path.join(imagesConfig.SVGPath, '*.svg'), { encoding: false })
+      .pipe(newer({ dest: imagesConfig.preprocessedPath }))
+      .pipe(svgMin((file) => {
+        if (path.matchesGlob(file.basename, 'russian_emblem.svg')) {
+          return {};
+        } else {
+          return {
+            floatPrecision: 0
+          }
+        }
+      }))
+      .pipe(imagemin([
+        svgo()
+      ]))
+      .pipe(dest(imagesConfig.preprocessedPath))
+  }
 );
 
 task('build:images',
-	series(
-		'build:images:russian_emblem.svg',
-		parallel(
-			series(
-				parallel(
-					'build:images:SVG2PNG',
-					'build:images:URL-QRCodes'
-				),
-				'build:images:preprocessing:PNG'
-			),
-			'build:images:preprocessing:SVG'
-		)
-	)
+  series(
+    'build:images:russian_emblem.svg',
+    parallel(
+      series(
+        parallel(
+          'build:images:SVG2PNG',
+          'build:images:URL-QRCodes'
+        ),
+        'build:images:preprocessing:PNG'
+      ),
+      'build:images:preprocessing:SVG'
+    )
+  )
 );
 
 task('clean:images',
-	function () {
-		return src(path.join(imagesConfig.preprocessedPath, '*.*'), { read: false, allowEmpty: true })
-			.pipe(clean())
-	}
+  function () {
+    return src(path.join(imagesConfig.preprocessedPath, '*.*'), { read: false, allowEmpty: true })
+      .pipe(clean())
+  }
 );
 
 //#endregion подготовка изображений
@@ -219,54 +229,54 @@ task('clean:images',
 const sourceORDTemplateSrcPath = path.join(sourceORDTemplatePath, 'src');
 
 task(
-	'build:template:ORD:Pictures',
-	() => {
-		const picturesPath = path.join(sourceORDTemplateSrcPath, 'Pictures');
-		return src(path.join(sourceORDTemplateSrcPath, 'META-INF/manifest.xml'), { encoding: false })
-			.pipe(through.obj(function (file, _, cb) {
+  'build:template:ORD:Pictures',
+  () => {
+    const picturesPath = path.join(sourceORDTemplateSrcPath, 'Pictures');
+    return src(path.join(sourceORDTemplateSrcPath, 'META-INF/manifest.xml'), { encoding: false })
+      .pipe(through.obj(function (file, _, cb) {
 
-				var self = this;
+        var self = this;
 
-				SaxonJS.getResource({ file: file.path, type: 'xml' })
-					.then((manifest) => {
-						return SaxonJS.XPath.evaluate(
-							`/manifest:manifest/manifest:file-entry[
+        SaxonJS.getResource({ file: file.path, type: 'xml' })
+          .then((manifest) => {
+            return SaxonJS.XPath.evaluate(
+              `/manifest:manifest/manifest:file-entry[
 										( @manifest:media-type='image/png' or @manifest:media-type='image/svg+xml' )
 										and starts-with(@manifest:full-path, 'Pictures/')
 									]/@manifest:full-path/string()`,
-							manifest,
-							{
-								namespaceContext: {
-									manifest: 'urn:oasis:names:tc:opendocument:xmlns:manifest:1.0'
-								},
-								resultForm: 'array'
-							}
-						)
-							.map((docPicturePath) => '**/' + path.basename(docPicturePath));
-					})
-					.then((docPictures) => {
-						src([path.join(imagesConfig.preprocessedPath, '*.*')], { encoding: false })
-							.pipe(filter(docPictures))
-							.on('end', cb)
-							.on('error', cb)
-							.on('data', (file) => { self.push(file); });
-					});
+              manifest,
+              {
+                namespaceContext: {
+                  manifest: 'urn:oasis:names:tc:opendocument:xmlns:manifest:1.0'
+                },
+                resultForm: 'array'
+              }
+            )
+              .map((docPicturePath) => '**/' + path.basename(docPicturePath));
+          })
+          .then((docPictures) => {
+            src([path.join(imagesConfig.preprocessedPath, '*.*')], { encoding: false })
+              .pipe(filter(docPictures))
+              .on('end', cb)
+              .on('error', cb)
+              .on('data', (file) => { self.push(file); });
+          });
 
-			}))
-			.pipe(newer(picturesPath))
-			.pipe(dest(picturesPath))
-	}
+      }))
+      .pipe(newer(picturesPath))
+      .pipe(dest(picturesPath))
+  }
 );
 
 task('build:template:ORD',
-	series(
-		'build:images',
-		'build:template:ORD:Pictures'
-	)
+  series(
+    'build:images',
+    'build:template:ORD:Pictures'
+  )
 );
 
 const _buildTemplates = parallel(
-	'build:template:ORD'
+  'build:template:ORD'
 );
 export { _buildTemplates as buildTemplates };
 
@@ -274,34 +284,34 @@ export { _buildTemplates as buildTemplates };
 
 //#region основные задачи
 task('build',
-	parallel(
-		'build:template:ORD'
-	)
+  parallel(
+    'build:template:ORD'
+  )
 );
 
 task('clean',
-	parallel(
-		'clean:images'
-	)
+  parallel(
+    'clean:images'
+  )
 );
 
 task('distclean',
-	series(
-		'clean'
-	)
+  series(
+    'clean'
+  )
 );
 
 task('maintainer-clean',
-	series(
-		'clean',
-		parallel(
-			'maintainer-clean:URL-QRCodes'
-		)
-	)
+  series(
+    'clean',
+    parallel(
+      'maintainer-clean:URL-QRCodes'
+    )
+  )
 );
 
 const _default = series(
-	'build'
+  'build'
 );
 export { _default as default };
 //#endregion основные задачи
